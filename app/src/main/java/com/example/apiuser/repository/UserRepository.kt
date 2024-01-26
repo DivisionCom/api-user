686c93aec253c7c3a3d8deb76916b0c3c20ff60a
DivisionCom <xd6511@gmail.com>
2024-01-26 22:41:28 +0300

    Add UserRepository class

---
+++ app/src/main/java/com/example/apiuser/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.example.apiuser.repository
+
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class UserRepository
+@Inject
+constructor() {
+
+}
