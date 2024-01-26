6b31c493a39909e91eb4ea63306cd69ac4c37f1b
DivisionCom <xd6511@gmail.com>
2024-01-26 22:19:14 +0300

    Add UserList Screen&ViewModel & Add start screen to Navigation

---
+++ app/src/main/java/com/example/apiuser/userlist/UserListScreen.kt
@@ -0,0 +1,11 @@
+package com.example.apiuser.userlist
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun UserListScreen(navController: NavController) {
+    Surface {
+    }
+}
