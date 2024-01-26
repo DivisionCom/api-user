b55e4568ef214f850195ce22f799c09e3ff5b5e8
DivisionCom <xd6511@gmail.com>
2024-01-26 21:45:26 +0300

    Add Roboto Font to whole project

--- app/src/main/java/com/example/apiuser/ui/theme/Type.kt
+++ app/src/main/java/com/example/apiuser/ui/theme/Type.kt
@@ -2,16 +2,30 @@ package com.example.apiuser.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.apiuser.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography =
     Typography(
         bodyLarge =
             TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = Roboto,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
                 lineHeight = 24.sp,
