1e8d2a8b0b31c2ab5686efbf68e5c5e829d92204
DivisionCom <xd6511@gmail.com>
2024-01-26 22:45:34 +0300

    Add interface UserApi, add it into constructor in Repository

---
+++ app/src/main/java/com/example/apiuser/data/remote/UserApi.kt
@@ -0,0 +1,4 @@
+package com.example.apiuser.data.remote
+
+interface UserApi {
+}
--- app/src/main/java/com/example/apiuser/repository/UserRepository.kt
+++ app/src/main/java/com/example/apiuser/repository/UserRepository.kt
@@ -1,11 +1,14 @@
 package com.example.apiuser.repository
 
+import com.example.apiuser.data.remote.UserApi
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class UserRepository
 @Inject
-constructor() {
+constructor(
+    private val api: UserApi,
+) {
 
 }
