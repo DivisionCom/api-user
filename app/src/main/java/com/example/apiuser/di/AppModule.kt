18f8b7c5a491b2670be0643f25d7cc6eebab380d
DivisionCom <xd6511@gmail.com>
2024-01-27 22:15:30 +0300

    Add OkHttp3 Client to the Retroft builder

--- app/src/main/java/com/example/apiuser/di/AppModule.kt
+++ app/src/main/java/com/example/apiuser/di/AppModule.kt
@@ -7,6 +7,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -22,7 +24,16 @@ object AppModule {
     @Singleton
     @Provides
     fun provideUserApi(): UserApi {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level =HttpLoggingInterceptor.Level.BODY
+
+        val client =
+            OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
         return Retrofit.Builder()
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
             .build()
