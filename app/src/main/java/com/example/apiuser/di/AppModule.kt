725909d15482dae9c49a648771e6954825cc5e42
DivisionCom <xd6511@gmail.com>
2024-01-27 22:13:41 +0300

    Add Retrofit Setup with BASE_URL

--- app/src/main/java/com/example/apiuser/di/AppModule.kt
+++ app/src/main/java/com/example/apiuser/di/AppModule.kt
@@ -1,9 +1,31 @@
 package com.example.apiuser.di
 
+import com.example.apiuser.data.remote.UserApi
+import com.example.apiuser.repository.UserRepository
+import com.example.apiuser.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideUserRepository(api: UserApi) = UserRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideUserApi(): UserApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(UserApi::class.java)
+    }
+}
