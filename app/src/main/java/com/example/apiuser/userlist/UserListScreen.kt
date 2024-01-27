443f01516f965ed0f6f2e06bd2aad0084df1f549
DivisionCom <xd6511@gmail.com>
2024-01-27 22:58:54 +0300

    Add ViewModel Setup with request

--- app/src/main/java/com/example/apiuser/userlist/UserListScreen.kt
+++ app/src/main/java/com/example/apiuser/userlist/UserListScreen.kt
@@ -2,10 +2,14 @@ package com.example.apiuser.userlist
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
-fun UserListScreen(navController: NavController) {
+fun UserListScreen(
+    navController: NavController,
+    viewModel: UserListViewModel = hiltViewModel(),
+) {
     Surface {
     }
 }
--- app/src/main/java/com/example/apiuser/userlist/UserListViewModel.kt
+++ app/src/main/java/com/example/apiuser/userlist/UserListViewModel.kt
@@ -1,3 +1,33 @@
 package com.example.apiuser.userlist
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.apiuser.repository.UserRepository
+import com.example.apiuser.util.Constants.API_RESULTS
+import com.example.apiuser.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
 class UserListViewModel
+    @Inject
+    constructor(
+        private val repository: UserRepository,
+    ) : ViewModel() {
+        init {
+            loadUsers()
+        }
+
+        fun loadUsers() {
+            viewModelScope.launch {
+                val result = repository.getUserList(API_RESULTS)
+                when (result) {
+                    is Resource.Success -> {
+                    }
+                    is Resource.Error -> {
+                    }
+                }
+            }
+        }
+    }
