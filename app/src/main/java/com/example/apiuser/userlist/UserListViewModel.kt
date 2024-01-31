320d5b53fd755104751e81da2d512b31856b52cd
DivisionCom <xd6511@gmail.com>
2024-01-31 21:28:28 +0300

    Add filling UserDetailEntries List

--- app/src/main/java/com/example/apiuser/userlist/UserListViewModel.kt
+++ app/src/main/java/com/example/apiuser/userlist/UserListViewModel.kt
@@ -3,6 +3,7 @@ package com.example.apiuser.userlist
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.apiuser.data.models.UserDetailEntry
 import com.example.apiuser.data.models.UserListEntry
 import com.example.apiuser.repository.UserRepository
 import com.example.apiuser.util.Constants.API_RESULTS
@@ -18,6 +19,7 @@ class UserListViewModel
         private val repository: UserRepository,
     ) : ViewModel() {
         val userList = mutableStateOf<List<UserListEntry>>(listOf())
+        val userDetail = mutableStateOf<List<UserDetailEntry>>(listOf())
         val loadError = mutableStateOf("")
         val isRefreshing = mutableStateOf(false)
 
@@ -41,9 +43,27 @@ class UserListViewModel
                                     city = entry.location.city,
                                 )
                             }
+                        val userDetailEntries =
+                            result.data?.results?.mapIndexed{ index, entry ->
+                                UserDetailEntry(
+                                    gender = entry.gender,
+                                    name = entry.name,
+                                    location = entry.location,
+                                    email = entry.email,
+                                    login = entry.login,
+                                    dob = entry.dob,
+                                    registered = entry.registered,
+                                    phone = entry.phone,
+                                    cell = entry.cell,
+                                    id = entry.id,
+                                    picture = entry.picture,
+                                    nat = entry.nat,
+                                )
+                            }
 
                         loadError.value = ""
                         userList.value = userEntries!!
+                        userDetail.value = userDetailEntries!!
                         isRefreshing.value = false
                     }
                     is Resource.Error -> {
