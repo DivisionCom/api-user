40fafc218a0525a8501dcc3d9726eab3699b3370
DivisionCom <xd6511@gmail.com>
2024-02-01 16:03:14 +0300

    Add UserDetailWrapper displaying as card

--- app/src/main/java/com/example/apiuser/userdetail/UserDetailScreen.kt
+++ app/src/main/java/com/example/apiuser/userdetail/UserDetailScreen.kt
@@ -9,17 +9,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -31,6 +36,8 @@ fun UserDetailScreen(
     userIndex: Int,
     navController: NavController,
     viewModel: UserDetailViewModel = hiltViewModel(),
+    userPhotoSize: Dp = 200.dp,
+    topPadding: Dp = 20.dp,
 ) {
     val userDetailList by remember {
         viewModel.userDetailList
@@ -56,6 +63,34 @@ fun UserDetailScreen(
                     .fillMaxHeight(0.2f)
                     .align(Alignment.TopCenter),
         )
+        UserDetailWrapper(
+            userIndex = userIndex,
+            userDetailList = userDetailList,
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = topPadding + userPhotoSize / 2f,
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 16.dp,
+                    )
+                    .shadow(10.dp, RoundedCornerShape(10.dp))
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(16.dp)
+                    .align(Alignment.BottomCenter),
+        )
+    }
+}
+
+@Composable
+fun UserDetailWrapper(
+    userIndex: Int,
+    userDetailList: List<UserDetailEntry>,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
     }
 }
 
