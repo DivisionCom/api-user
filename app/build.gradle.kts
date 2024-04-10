0d59d3a5ac41c1fc72392c65f864383d2f0e0321
DivisionCom <xd6511@gmail.com>
2024-04-10 21:50:28 +0300

    Add junit-jupiter library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,6 +103,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
